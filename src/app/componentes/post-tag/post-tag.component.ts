72550cbab1064c1aef56d3b735fdecc70188af7a
Vinicius Baumann Ladosky <vinicius.ladosky@gmail.com>
2024-03-14 16:08:08 -0300

    post-tag criado

---
+++ src/app/componentes/post-tag/post-tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-tag',
+  templateUrl: './post-tag.component.html',
+  styleUrls: ['./post-tag.component.css']
+})
+export class PostTagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
